134d215aaa8d9e4b7e0027b295aa1183e70cc594
Darek Dwornikowski <dariusz.dwornikowski@gmail.com>
2025-11-05 21:11:59 +0100

    vercel integrationb

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://bitropy.com",
   integrations: [mdx(), sitemap(), icon()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
